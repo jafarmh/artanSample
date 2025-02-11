7d6be347e0cc79428258e09b39228a6b185221ff
jafarmh <jafar.m@yahoo.com>
2025-02-11 21:11:01 +0330

    add api test data

---
+++ app/api/content/route.js
@@ -0,0 +1,87 @@
+let data = [
+  {
+    "id": 1,
+    "title": "Title 1",
+    "content": "Content 1",
+    "date": "1403-11-23"
+  },
+  {
+    "id": 2,
+    "title": "Title 2",
+    "content": "Content 2",
+    "date": "1403-11-23"
+  },
+  {
+    "id": 3,
+    "title": "Title 3",
+    "content": "Content 3",
+    "date": "1403-11-23"
+  }
+];
+
+const toEn = num => {
+  const id = {
+    '۰': '0',
+    '۱': '1',
+    '۲': '2',
+    '۳': '3',
+    '۴': '4',
+    '۵': '5',
+    '۶': '6',
+    '۷': '7',
+    '۸': '8',
+    '۹': '9',
+  }
+  return num ? num.toString().replace(/[^0-9.]/g, function (w) {
+    return id[w] || w
+  }) : null
+}
+export async function GET(req) {
+  return Response.json(data);
+}
+
+
+export async function POST(req) {
+  const body = await req.json();
+
+  const now = new Date();
+
+  data.push({
+    id: data.length + 1,
+    title: body.title,
+    content: body.content,
+    date: toEn(now.toLocaleDateString('fa').replaceAll("/", "-")),
+  });
+  return Response.json(data);
+}
+
+export async function PUT(req) {
+  const body = await req.json();
+
+  let index = data.findIndex((item) => item.id === body.id);
+  if (index >= 0) {
+    data[index]['title'] = body.title;
+    data[index]['content'] = body.content;
+
+    return Response.json(data);
+  } else {
+    return Response.json({ "no data found": true });
+
+  }
+}
+export async function DELETE(req) {
+  const body = await req.json();
+
+  let index = data.findIndex((item) => item.id === body.id);
+  if (index >= 0) {
+    delete data[index];
+
+    return Response.json(data);
+  } else {
+    return Response.json({ "no data found": true });
+
+  }
+
+
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,47 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/content");
+  const data = await res.json();
+
+  const resPost = await fetch("http://localhost:3000/api/content", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ 
+      title:"add with post",
+      content:"for test"
+     }),
+  });
+  const dataPost = await resPost.json();
+
+  console.log(dataPost,'============data')
+
+  const resPUT = await fetch("http://localhost:3000/api/content", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ 
+      id:2,
+      title:"update with PUT",
+      content:"for test edit"
+     }),
+  });
+  const dataPUT = await resPUT.json();
+
+  console.log(dataPUT,'============dataPUT')
+
+
+  
+  const resDelete = await fetch("http://localhost:3000/api/content", {
+    method: "Delete",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ 
+      id:3,
+      
+     }),
+  });
+  const dataDelete = await resDelete.json();
+
+  console.log(dataDelete,'============dataDelete')
   return (
     <div className=" ">
     
